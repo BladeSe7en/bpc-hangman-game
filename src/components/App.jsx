9477a785e92394e92145f9dee7263cae06afe290
BrianC <seahorse8789@gmail.com>
2018-10-16 09:39:23 -0700

    added a defeat screen when a player runs out of strikes. added a button to select another random word from the same catagory. or added an inputand button to change the catagory.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,23 +8,24 @@ export default class MainPage extends Component {
     super(props);
 
     this.state = {
-      isMainPageShowing: true,
-      isGameShowing: false,
-      isScoreShowing: false,
-      allGuesses: [],
-      currentGuess: [],
-      currentWord: [],
-      letter: '',
-      catagory: ''
+      allGuesses        : [],
+      catagory          : '',
+      currentGuess      : [],
+      currentWord       : [],
+      data              : [],
+      isGameShowing     : false,
+      isMainPageShowing : true,
+      isScoreShowing    : false,
+      letter            : '',
     };
-    this.clickHandler = this.clickHandler.bind(this);
-    this.currentWord = this.currentWord.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    this.setState({ catagory: e.target.value.toLowerCase() });
-  }
 
+    this.clickHandler   = this.clickHandler.bind(this);
+    this.currentWord    = this.currentWord.bind(this);
+    this.handleChange   = this.handleChange.bind(this);
+    this.handleState    = this.handleState.bind(this);
+
+  }
+  
   clickHandler() {
     console.log('click handler was clicked');
     this.currentWord();
@@ -33,31 +34,46 @@ export default class MainPage extends Component {
       isGameShowing: true,
     });
   }
+
   currentWord() {
     var topic = this.state.catagory;
     console.log('this is topic ', topic);
     axios.get(`https://api.datamuse.com/words?topics=${topic}`)
-      .then(response => {
-        var index = Math.floor(Math.random() * response.data.length);
-        console.log('this is index: ', index);
-        console.log('this is data: ', response.data[index]);
-        console.log('this is length: ', response.data.length);
-        this.setState({
-          currentWord: response.data[index].word.split('')
-        });
-
-      })
-      .catch((err) => {
-        console.log(err);
-        response.send(err);
+    .then(response => {
+      var index = Math.floor(Math.random() * response.data.length);
+      console.log('this is index: ', index);
+      console.log('this is data: ', response.data[index]);
+      console.log('this is length: ', response.data.length);
+      this.setState({
+        currentWord: response.data[index].word.split(''),
+        data: response.data
       });
+      
+    })
+    .catch((err) => {
+      console.log(err);
+      response.send(err);
+    });
+  }
+  
+  handleChange(e) {
+    this.setState({ catagory: e.target.value.toLowerCase() });
+  }
+
+  handleState(word)  {
+    console.log('this is word: ',word);
+    this.setState({currentWord: word});
+    console.log('this is currentword after setState: ',this.state.currentWord);
   }
 
   render() {
     const currentWord = this.state.currentWord;
     if (!this.state.isMainPageShowing) {
       return <GamePage
-        currentWord={currentWord} />;
+        currentWord       ={currentWord}
+        data              ={this.state.data}
+        handleCurrentWord ={this.handleCurrentWord}
+        handleState       ={this.handleState}/>
     }
 
     return (
@@ -66,18 +82,14 @@ export default class MainPage extends Component {
           <img id="hangman-title-img" src="https://occ-0-901-1001.1.nflxso.net/art/87e01/5694568c69ef4be79164f46b967e7f4c1a387e01.png" />
           <h1 className="title">Instructions</h1>
           <p className="rules">The objective is simple. You must guess your word correctly before the full charactor is drawn. On the 6th wrong answer you will lose. At the cost of one move you may click the hint button to recieve a clue about the word </p>
-          <p>To begin, please enter in a chatagory.</p>
+          <p>To begin, please enter in a catagory.</p>
         </div>
         <button className="btn" onClick={this.clickHandler}>start game</button>
         <input
-          className="catagory"
-          value={this.state.catagory}
-          onChange={this.handleChange} />
+          className ="catagory"
+          value     ={this.state.catagory}
+          onChange  ={this.handleChange} />
 
-        {currentWord && currentWord.map(word => {
-          console.log(word);
-          return (_);
-        })}
       </div>
 
     )
@@ -90,198 +102,271 @@ export class GamePage extends Component {
     super(props);
 
     this.state = {
-      isMainPageShowing: true,
-      isGameShowing: false,
-      isScoreShowing: false,
-      correctGuesses: [],
-      allGuesses: [],
-      letter: '',
-      strike: 0,
-      isGameOver: false
+      isMainPageShowing : true,
+      isGameShowing     : false,
+      isScoreShowing    : false,
+      correctGuesses    : [],
+      allGuesses        : [],
+      letter            : '',
+      strike            : 0,
+      isGameOver        : false,
+      wrongGuessesLeft  : 6,
+      catagory          : ''
     }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit         = this.handleSubmit.bind(this);
+    this.sameCatagoryNewWord  = this.sameCatagoryNewWord.bind(this);
+    this.handleChange         = this.handleChange.bind(this);
+    this.clickHandler         = this.clickHandler.bind(this);
+
+
   }
-  // sameCatagoryNewWord() {
-  // var index = Math.floor(Math.random() * response.data.length);
-  // console.log('this is index: ', index);
-  // console.log('this is data: ', response.data[index]);
-  // console.log('this is length: ', response.data.length);
-  // this.setState({
-  //   currentWord: response.data[index].word.split('')
-  // }
-  // isGameOver(strike) {
-  //   if (this.state.strike === 6) {
-  //     this.render(
-  //     return (
-  //       <div>
-  //         <h1>GAME OVER! WOULD YOU LIKE TO TRY AGAIN?</h1>
-  //         </div>
-  //       <button className="btn" onClick={this.clickHandler}>SAME CATAGORY</button>
-  //       <input
-  //         className="catagory"
-  //         value={this.state.catagory}
-  //         onChange={this.handleChange} />
-  //         <button className="btn" onClick={this.clickHandler}>NEW CATAGORY</button>
-  //       </div>
-  //     ))
-  //   }
-  // }
 
-  handleSubmit(currentGuess) {
-    //this function checks to see if the current word includes the letter that the user guessed.
-    //if the guess is in the current word it is updating the state of correct guesses by adding the current guess to the correct guesses array
-    //it is also updating all guesses by adding each current guess to the array regardless if the guess is correct or not
-    //if the current guess is not apart of the current word it updates the state of strike by adding one to the count
-    //and it also updates the all guesses state by adding the current guess in the array
+  handleChange(e) {
+    
+    this.setState({ catagory: e.target.value.toLowerCase() });
+  }
 
-    if (this.props.currentWord.includes(currentGuess)) {
+  currentWord() {
+    var topic = this.state.catagory;
+    console.log('this is topic ', topic);
+    axios.get(`https://api.datamuse.com/words?topics=${topic}`)
+      .then(response => {
+        var index = Math.floor(Math.random() * response.data.length);
+        console.log('this is index: ', index);
+        console.log('this is data: ', response.data[index]);
+        console.log('this is length: ', response.data.length);
+        this.setState({
+          allGuesses        : [],
+          correctGuesses    : [],
+          currentWord       : response.data[index].word.split(''),
+          data              : response.data,
+          isGameOver        : false,
+          letter            : '',
+          strike            : 0,
+          wrongGuessesLeft  : 6,
+        });
+
+      })
+      .catch((err) => {
+        console.log(err);
+        response.send(err);
+      });
+  }
+
+  clickHandler() {
+    console.log('click handler was clicked');
+    this.currentWord();
+    this.setState({
+      isGameOver: false,
+    });
+  }
+
+  sameCatagoryNewWord() {
+    var index = Math.floor(Math.random() * this.props.data.length);
+    console.log('this is index: ', index);
+    console.log('this is data: ', this.props.data[index]);
+    console.log('this is length: ', this.props.data.length);
+    this.setState({
+      allGuesses        : [],
+      correctGuesses    : [],
+      isGameOver        : false,
+      letter            : '',
+      strike            : 0,
+      wrongGuessesLeft  : 6,
+    });
+
+    this.props.handleState(this.props.data[index].word.split(''))
+    console.log('this is currentWord in gamepage: ', this.props.handleState(this.props.data[index].word.split('')));
+  }
+
+  isGameOver(n) {
+    console.log('testing isGameOver')
+    if (n == 6) {
+      console.log('strike is at 6, should update state now')
       this.setState({
-        correctGuesses: this.state.correctGuesses.concat(currentGuess),
-        allGuesses: this.state.allGuesses.concat(currentGuess)
+        isGameOver: true
       })
     }
-    else {
-      this.setState({
-        strike: this.state.strike + 1,
-        allGuesses: this.state.allGuesses.concat(currentGuess)
-      })
-    }
-  }
-  letterImg(letter) {
-    if (letter.toLowerCase() === "a") {
-      return "A"
-    }
-    if (letter.toLowerCase() === "b") {
-      return 'B'
-    }
-    if (letter.toLowerCase() === "c") {
-      return 'C'
-    }
-    if (letter.toLowerCase() === "d") {
-      return 'D'
-    }
-    if (letter.toLowerCase() === "e") {
-      return 'E'
-    }
-    if (letter.toLowerCase() === "f") {
-      return 'F'
-    }
-    if (letter.toLowerCase() === "g") {
-      return 'G'
-    }
-    if (letter.toLowerCase() === "h") {
-      return 'H'
-    }
-    if (letter.toLowerCase() === "i") {
-      return 'I'
-    }
-    if (letter.toLowerCase() === "j") {
-      return 'J'
-    }
-    if (letter.toLowerCase() === "k") {
-      return 'K'
-    }
-    if (letter.toLowerCase() === "l") {
-      return 'L'
-    }
-    if (letter.toLowerCase() === "m") {
-      return 'M'
-    }
-    if (letter.toLowerCase() === "n") {
-      return 'N'
-    }
-    if (letter.toLowerCase() === "o") {
-      return 'O'
-    }
-    if (letter.toLowerCase() === "p") {
-      return 'P'
-    }
-    if (letter.toLowerCase() === "q") {
-      return 'Q'
-    }
-    if (letter.toLowerCase() === "r") {
-      return 'R'
-    }
-    if (letter.toLowerCase() === "s") {
-      return 'S'
-    }
-    if (letter.toLowerCase() === 't') {
-      return 'T'
-    }
-    if (letter.toLowerCase() === "u") {
-      return 'U'
-    }
-    if (letter.toLowerCase() === "v") {
-      return 'V'
-    }
-    if (letter.toLowerCase() === "w") {
-      return 'W'
-    }
-    if (letter.toLowerCase() === "x") {
-      return 'X'
-    }
-    if (letter.toLowerCase() === "y") {
-      return 'Y'
-    }
-    if (letter.toLowerCase() === "z") {
-      return 'Z'
-    }
   }
 
-  render() {
+    handleSubmit(currentGuess) {
+      //this function checks to see if the current word includes the letter that the user guessed.
+      //if the guess is in the current word it is updating the state of correct guesses by adding the current guess to the correct guesses array
+      //it is also updating all guesses by adding each current guess to the array regardless if the guess is correct or not
+      //if the current guess is not apart of the current word it updates the state of strike by adding one to the count
+      //and it also updates the all guesses state by adding the current guess in the array
 
-    var img = <img id="hangman-0" src="https://cdn.discordapp.com/attachments/374257557880963072/492878824501936149/hangman-0.png" />;
-    var letter;
-
-    if (this.state.strike === 1) {
-      img = <img id="hangman-1" src="https://media.discordapp.net/attachments/374257557880963072/492878854529089547/hangman-1.png" />
-    }
-    if (this.state.strike === 2) {
-      img = <img id="hangman-2" src="https://media.discordapp.net/attachments/374257557880963072/492878881896923150/hangman-2.png" />
-    }
-    if (this.state.strike === 3) {
-      img = <img id="hangman-3" src="https://media.discordapp.net/attachments/374257557880963072/492878930995576832/hangman-3.png" />
-    }
-    if (this.state.strike === 4) {
-      img = <img id="hangman-4" src="https://media.discordapp.net/attachments/374257557880963072/492878991133376523/hangman-4.png" />
-    }
-    if (this.state.strike === 5) {
-      img = <img id="hangman-5" src="https://media.discordapp.net/attachments/374257557880963072/492879021600669716/hangman-5.png" />
-    }
-    if (this.state.strike === 6) {
-      img = <img id="hangman-6" src="https://media.discordapp.net/attachments/374257557880963072/492879053070532618/hangman-6.png" />
-    }
-
-
-    return (
-      <div>
-        <div>
-          <img id="title-2" src="https://cdn.discordapp.com/attachments/374257557880963072/492551101883875349/title2.png" />
-          <div className="all-guesses">
-            {this.state.allGuesses && this.state.allGuesses.map(letter => {
-              //instead of returning span, invoke that function that you created, and return that
-              return this.letterImg(letter)
-            })}
-          </div>
-        </div>
-
+      if (this.props.currentWord.includes(currentGuess)) {
+        this.setState({
+          allGuesses      : this.state.allGuesses.concat(currentGuess),
+          correctGuesses  : this.state.correctGuesses.concat(currentGuess),
+        })
+      }
+      else {
+        this.isGameOver( this.state.strike + 1);
+        this.setState({
+          allGuesses        : this.state.allGuesses.concat(currentGuess),
+          strike            : this.state.strike + 1,
+          wrongGuessesLeft  : (+(this.state.wrongGuessesLeft)-1),
+        })
+      }
       
+    }
+    letterImg(letter) {
+      if (letter.toLowerCase() === "a") {
+        return "A"
+      }
+      if (letter.toLowerCase() === "b") {
+        return 'B'
+      }
+      if (letter.toLowerCase() === "c") {
+        return 'C'
+      }
+      if (letter.toLowerCase() === "d") {
+        return 'D'
+      }
+      if (letter.toLowerCase() === "e") {
+        return 'E'
+      }
+      if (letter.toLowerCase() === "f") {
+        return 'F'
+      }
+      if (letter.toLowerCase() === "g") {
+        return 'G'
+      }
+      if (letter.toLowerCase() === "h") {
+        return 'H'
+      }
+      if (letter.toLowerCase() === "i") {
+        return 'I'
+      }
+      if (letter.toLowerCase() === "j") {
+        return 'J'
+      }
+      if (letter.toLowerCase() === "k") {
+        return 'K'
+      }
+      if (letter.toLowerCase() === "l") {
+        return 'L'
+      }
+      if (letter.toLowerCase() === "m") {
+        return 'M'
+      }
+      if (letter.toLowerCase() === "n") {
+        return 'N'
+      }
+      if (letter.toLowerCase() === "o") {
+        return 'O'
+      }
+      if (letter.toLowerCase() === "p") {
+        return 'P'
+      }
+      if (letter.toLowerCase() === "q") {
+        return 'Q'
+      }
+      if (letter.toLowerCase() === "r") {
+        return 'R'
+      }
+      if (letter.toLowerCase() === "s") {
+        return 'S'
+      }
+      if (letter.toLowerCase() === 't') {
+        return 'T'
+      }
+      if (letter.toLowerCase() === "u") {
+        return 'U'
+      }
+      if (letter.toLowerCase() === "v") {
+        return 'V'
+      }
+      if (letter.toLowerCase() === "w") {
+        return 'W'
+      }
+      if (letter.toLowerCase() === "x") {
+        return 'X'
+      }
+      if (letter.toLowerCase() === "y") {
+        return 'Y'
+      }
+      if (letter.toLowerCase() === "z") {
+        return 'Z'
+      }
+    }
 
-      <div className="hangman">
-        {img}
+    render() {
+      if (this.state.isGameOver){
+        return ( 
+        <div className='defeat'>
+        <div>
+          <h1>GAME OVER! WOULD YOU LIKE TO TRY AGAIN?</h1>
+        </div>
+        <button className="btn" onClick={this.sameCatagoryNewWord}>SAME CATAGORY</button>
+        <input
+          className="catagory"
+          placeholder="new catagory"
+          value={this.state.catagory}
+          onChange={this.handleChange} />
+        <button className="btn" onClick={this.clickHandler}>NEW CATAGORY</button>
       </div>
-      <Word
-        correctGuesses={this.state.correctGuesses}
-        currentWord={this.props.currentWord}
-        handleSubmit={this.handleSubmit}
-      />
-      </div >
+      
+        )}
+      var img = <img id="hangman-0" src="https://cdn.discordapp.com/attachments/374257557880963072/492878824501936149/hangman-0.png" />;
+      var letter;
+
+      if (this.state.strike === 1) {
+        img = <img id="hangman-1" src="https://media.discordapp.net/attachments/374257557880963072/492878854529089547/hangman-1.png" />
+      }
+      if (this.state.strike === 2) {
+        img = <img id="hangman-2" src="https://media.discordapp.net/attachments/374257557880963072/492878881896923150/hangman-2.png" />
+      }
+      if (this.state.strike === 3) {
+        img = <img id="hangman-3" src="https://media.discordapp.net/attachments/374257557880963072/492878930995576832/hangman-3.png" />
+      }
+      if (this.state.strike === 4) {
+        img = <img id="hangman-4" src="https://media.discordapp.net/attachments/374257557880963072/492878991133376523/hangman-4.png" />
+      }
+      if (this.state.strike === 5) {
+        img = <img id="hangman-5" src="https://media.discordapp.net/attachments/374257557880963072/492879021600669716/hangman-5.png" />
+      }
+      if (this.state.strike === 6) {
+        img = <img id="hangman-6" src="https://media.discordapp.net/attachments/374257557880963072/492879053070532618/hangman-6.png" />
+      }
 
 
-    )
+      return (
+        <div>
+          <div>
+            <img id="title-2" src="https://cdn.discordapp.com/attachments/374257557880963072/492551101883875349/title2.png" />
+            <div className="all-guesses">
+              {this.state.allGuesses && this.state.allGuesses.map(letter => {
+                //instead of returning span, invoke that function that you created, and return that
+                return this.letterImg(letter)
+              })}
+            </div>
+          </div>
+          <div className='guesses-left'>
+           <h1>strikes left: {this.state.wrongGuessesLeft}</h1>
+          </div>
+
+
+          {/* <div>
+            {return this.isGameOver()}
+        </div> */}
+          <div className="hangman">
+            {img}
+          </div>
+          <Word
+            correctGuesses={this.state.correctGuesses}
+            currentWord={this.props.currentWord}
+            handleSubmit={this.handleSubmit}
+          />
+        </div >
+
+
+      )
+    }
   }
-}
 export class Word extends Component {
   constructor(props) {
     super(props);
