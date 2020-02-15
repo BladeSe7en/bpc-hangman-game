4f1fd400cd735497ac6359611e52ffa156899f32
BrianC <seahorse8789@gmail.com>
2020-02-14 21:46:54 -0800

    added an error message to the get request when data is returned undefined

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './Components/App';
+import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
